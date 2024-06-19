2c0e22530bc2a739da1e1c7439db21987f4eff8f
CharlyFernando <159191409+CharlyFernando@users.noreply.github.com>
2024-06-19 15:58:44 -0600

    actualizado

--- app.py
+++ app.py
@@ -1,43 +1,81 @@
-from flask import Flask, request, render_template,redirect,flash, session
+from flask import Flask, request, render_template, redirect, flash, session
 from models import Usuario, db
-#con eso importamos la password hasheada
+# con eso importamos la password hasheada
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
 from flask_session import Session
+# clase de Integrity Error
+from sqlalchemy.exc import IntegrityError
+# importaciones para login
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
-app= Flask(__name__)
+app = Flask(__name__)
 app.config.from_object(Config)
 
-app.config['SESSION_PERMANENT']= False
-app.config['SESSION_TYPE']= 'filesystem'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 
 db.init_app(app)
 
-@app.route("/") #decorador
-def index():
-    name = "Gabo"
-    edad = "18"
-    dinero = -225
+#configuraciones para logearse
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
-    
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Has cerrado la sesion", 'success')
+    return redirect("/")
+
+@app.route("/") # esto es un decorador
+@login_required
+def index():
+    name = "Gabriel"
+    edad = 25
+
+    dinero = 200
+
     return render_template("index.html", name=name, edad=edad, dinero=dinero)
 
 @app.route("/saludo/<nombre>")
 def saludo(nombre):
     return f"Hola, {nombre}"
-
-
-
 # LOGIN
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        name = request.form.get("name")
-        print(f"Hola, {name}")
-        return f"Hola {name}"
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        if not email or not password:
+            flash("Campos vacios", "danger")
+            return redirect("/login")
+        
+        usuario = Usuario.query.filter_by(email_user=email).first()
+        
+        if usuario is None:
+            flash("Usuario no encontrado", 'danger')
+            return redirect("/login")
+
+        print(f"U: {usuario.password_hash}")
+
+        if not check_password_hash(usuario.password_hash, password):
+            flash("Contraseñas no coinciden", 'warning')
+            return redirect("/login")
+        
+        # si todos los campos y validaciones estan correctos
+        login_user(usuario)
+        flash("Inicio de sesion exitoso", 'success')
+        return redirect("/")
     else:
-        return render_template("index.html")
+        return render_template("login.html")
     
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -46,33 +84,39 @@ def register():
         email = request.form.get("email")
         password = request.form.get("password")
 
-        #si los campos estan vacios
-        if not username or not password or not email:
+        print(f"{username, password}")
+        # si los campos estan vacios
+        if not username or not password:
+            # es danger, porque es para errores
             flash("Campos vacios", "danger")
             return redirect("/register")
         
-        #creamos un nuevo usuario
-        #creamos una password segura
+        # creamos un nuevo usuario
+        # crear una password segura
         password_hash = generate_password_hash(password)
+            
+        try: 
+            # si el usuario es nuevo
+            new_user = Usuario(username=username, password_hash=password_hash, email_user=email)
 
-        #si el usuario ya existe
-        usuario_existente = Usuario.query.filter_by(username=username).first()
-
-        if usuario_existente:
-            flash("El usuario ya existe", "danger")
+            # guardamos a la base de datos con db
+            db.session.add(new_user)
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            flash("El usuario ya existe", 'danger')
             return redirect("/register")
-        
-        #si el usuario es nuevo
-        new_user = Usuario(username=username, password_hash= password_hash, email_user=email)
 
-        #guardamos a la base de datos con db
-        db.session.add(new_user)
-        db.session.commit()
-
-        flash("Usuario registrado!","sucess")
+        flash("Usuario registrado!", 'success ')
         return redirect("/")
+
     else:
         return render_template("register.html")
 
-if __name__=='__main__':
+@app.route("/about")
+@login_required
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,14 +1,54 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
 
 db = SQLAlchemy()
 
 class Usuario(db.Model):
-    #Campos de la tabla/modelo
-    id= db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    email_user = db.Column(db.String(80), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
+    email_user = db.Column(db.String(60), unique=True, nullable=False)
 
-    #formatear datos, imprimirlos
     def __repr__(self):
-        return f"username: {self.username} Email: {self.email_user}"
+        return f"User: {self.username} Email: {self.email_user}"
+    
+    # Metodos por flask_login
+    @property
+    def is_authenticated(self):
+        return True
+    
+    @property
+    def is_active(self):
+        return True
+    
+    @property
+    def is_anonymous(self):
+        return False
+    
+    def get_id(self):
+        return str(self.id)
+
+
+class Persona(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth = db.Column(db.Integer, nullable=False)
+    directors = db.relationship('Director', backref='persona', lazy='dynamic')
+    actors = db.relationship('Actor', backref='persona', lazy='dynamic')
+
+class Director(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('persona.id'), nullable=False)
+
+class Actor(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'), nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey('persona.id'), nullable=False)
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False)
+    directors = db.relationship('Director', backref='movie', lazy='dynamic')
+    actors = db.relationship('Actor', backref='movie', lazy='dynamic')
